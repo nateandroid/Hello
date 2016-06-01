ef214daa43957d7b37cfe59b18ac4eccc4aeaec5
renshuang <ok@qq.com>
2016-06-01 22:07:15 +0800

    修改mainactivity

--- app/src/main/java/nate/hello/MainActivity.java
+++ app/src/main/java/nate/hello/MainActivity.java
@@ -3,16 +3,6 @@ package nate.hello;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import www.ioffers.com.GitHub;
-
 public class MainActivity extends AppCompatActivity {
 
     public static final String API_URL = "https://api.github.com/square/retrofit";
@@ -27,38 +17,38 @@ public class MainActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_main);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(API_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-
-
-
-        GitHub github = retrofit.create(GitHub.class);
-        Call<List<Contributor>> call = github.contributors("square", "retrofit");
-
-
-
-
-
-        call.enqueue(new Callback<List<Contributor>>() {
-            @Override
-            public void onResponse(Call<List<Contributor>> call, Response<List<Contributor>> response) {
-                for (Contributor contributor : response.body()) {
-                    System.out.println(contributor.login + " (" + contributor.contributions + ")");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Contributor>> call, Throwable t) {
-
-            }
-
-
-        });
-        System.out.println(call.request().url());
+//
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl(API_URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//
+//
+//
+//
+//        GitHub github = retrofit.create(GitHub.class);
+//        Call<List<Contributor>> call = github.contributors("square", "retrofit");
+//
+//
+//
+//
+//
+//        call.enqueue(new Callback<List<Contributor>>() {
+//            @Override
+//            public void onResponse(Call<List<Contributor>> call, Response<List<Contributor>> response) {
+//                for (Contributor contributor : response.body()) {
+//                    System.out.println(contributor.login + " (" + contributor.contributions + ")");
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<Contributor>> call, Throwable t) {
+//
+//            }
+//
+//
+//        });
+//        System.out.println(call.request().url());
 
 
     }
