49d48ee2d2c022714f97271693d84a0a68b003fb
renshuang <ok@qq.com>
2016-06-03 21:58:12 +0800

    add blank

--- app/src/main/java/nate/hello/MainActivity.java
+++ app/src/main/java/nate/hello/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_main);
+
 //
 //        Retrofit retrofit = new Retrofit.Builder()
 //                .baseUrl(API_URL)
